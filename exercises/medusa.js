586bbe3335b18c2a7d8f0d70c7e5af9853423832
Kyle Williams <kylecharleswilliams@gmail.com>
2020-02-09 23:25:20 -0700

    Remove if statements from medusa

--- exercises/medusa.js
+++ exercises/medusa.js
@@ -7,13 +7,15 @@ class Medusa {
     this.stare = function(victim) {
       this.statues.push(victim);
       this.statues[vicNum].stoned = true;
+      var vicBool = !!(vicNum > 2);
       vicNum++
-      if (vicNum > 3){
-        this.pastVictims.push(this.statues[0])
-        this.pastVictims.stoned = false;
-        this.statues.shift();
+      var num1 = [0,1];
+      var num2 = [1,4];
+      var retainArray1 = [0,1];
+      this.statues = this.statues.slice(retainArray1[vicBool*1],4);
+      this.pastVictims.push( this.statues.slice(num1[vicBool*1],num2[vicBool*1]));
+      this.pastVictims.stoned = !!!(this.statues.length > 2)
       }
-    }
   }
 }
 module.exports = Medusa;
